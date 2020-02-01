55c286de66fbb5d81336787105840709545bcadd
Jiayuan Mao <maojiayuan@gmail.com>
2020-02-01 11:36:32 -0500

    [master] support search now

--- mldash/web/app/index.py
+++ mldash/web/app/index.py
@@ -8,6 +8,7 @@
 # This file is part of Jacinle.
 # Distributed under terms of the MIT license.
 
+import json
 from jacweb.web import route, JacRequestHandler
 from mldash.data.orm import init_database, ProjectMetainfo, Desc
 from mldash.web.custom_pages import get_custom_pages
@@ -20,4 +21,24 @@ class IndexHandler(JacRequestHandler):
         descs = Desc.select().execute()
         kwargs = {'metainfo': metainfo, 'title': metainfo['title'], 'descs': descs, 'custom_pages': get_custom_pages()}
         self.render('index.html', **kwargs)
+
+
+@route(r'/api/search-list')
+class APISearchListHandler(JacRequestHandler):
+    def get(self):
+        self.set_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        descs = Desc.select().execute()
+
+        outputs = list()
+        for desc in descs:
+            outputs.append(f'#desc/{desc.desc_name}')
+        for desc in descs:
+            for expr in desc.exprs:
+                outputs.append(f'#desc/{desc.desc_name}/expr/{expr.expr_name}')
+        for desc in descs:
+            for expr in desc.exprs:
+                for run in expr.runs:
+                    outputs.append(f'#desc/{desc.desc_name}/expr/{expr.expr_name}/run/{run.run_name}')
+
+        self.write(json.dumps(outputs))
 
